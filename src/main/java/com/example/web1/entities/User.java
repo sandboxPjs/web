d09ed7f762a1ba3b13f1680f387d8bc173aef3e9
Dmitry Shishagin <dmitry.shishagin@mail.ru>
2022-11-11 11:59:33 +0600

    added input form for user name on the greeting page

---
+++ src/main/java/com/example/web1/entities/User.java
@@ -0,0 +1,15 @@
+package com.example.web1.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/example/web1/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/web1/servingwebcontent/GreetingController.java
@@ -1,9 +1,10 @@
 package com.example.web1.servingwebcontent;
 
+import com.example.web1.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
@@ -11,6 +12,15 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("user", new User());
+
+        return "greeting";
+    }
+
+    @RequestMapping(value = "/submitName", method = RequestMethod.POST)
+    public String submitName(@ModelAttribute User user, BindingResult errors, Model model) {
+        model.addAttribute("name", user.getName());
+
         return "greeting";
     }
 
